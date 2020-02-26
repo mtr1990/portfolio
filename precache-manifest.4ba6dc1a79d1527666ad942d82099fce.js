f6f8855ed845d48fc0e44306f395f6f6a077dd3c
Mai Trịnh <mrlufydl@gmail.com>
2020-02-26 15:01:54 +0700

    Updates

---
+++ precache-manifest.4ba6dc1a79d1527666ad942d82099fce.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2191c4f02eeee25d772e69e8744dbf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b8b6ab7dee298839bc3f",
+    "url": "/portfolio/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "c0853d884242df388f11",
+    "url": "/portfolio/static/css/main.447a239a.chunk.css"
+  },
+  {
+    "revision": "b8b6ab7dee298839bc3f",
+    "url": "/portfolio/static/js/2.c99446ac.chunk.js"
+  },
+  {
+    "revision": "52d6c37f800c4219da28",
+    "url": "/portfolio/static/js/3.0cdded8a.chunk.js"
+  },
+  {
+    "revision": "1349cf52b11577a70463",
+    "url": "/portfolio/static/js/4.d1e69e76.chunk.js"
+  },
+  {
+    "revision": "ace21a6a062f32186e1c",
+    "url": "/portfolio/static/js/5.ea7d1f9d.chunk.js"
+  },
+  {
+    "revision": "7d01c2e2894d761d6f0d",
+    "url": "/portfolio/static/js/6.bbe36165.chunk.js"
+  },
+  {
+    "revision": "c0853d884242df388f11",
+    "url": "/portfolio/static/js/main.0b41f090.chunk.js"
+  },
+  {
+    "revision": "0a0152d6cefc0f3063db",
+    "url": "/portfolio/static/js/runtime~main.414ac0d3.js"
+  },
+  {
+    "revision": "725e574b19244619bc76fb9b4a1b3d94",
+    "url": "/portfolio/static/media/bg-body-dark.725e574b.jpg"
+  },
+  {
+    "revision": "446264c18c418b68a2987d435eab87e1",
+    "url": "/portfolio/static/media/bg-body-light.446264c1.jpg"
+  },
+  {
+    "revision": "092c564a870a690c326251f961461419",
+    "url": "/portfolio/static/media/bg-login.092c564a.jpg"
+  },
+  {
+    "revision": "758564163070fb263b20079ebab2e4d9",
+    "url": "/portfolio/static/media/ic-loading.75856416.svg"
+  },
+  {
+    "revision": "8140e6c1af0a3dfd4cb2e6688ef891c1",
+    "url": "/portfolio/static/media/ic-me.8140e6c1.svg"
+  },
+  {
+    "revision": "c1c830fa928372d6fd5d39acf0083f8a",
+    "url": "/portfolio/static/media/ic-moon.c1c830fa.svg"
+  },
+  {
+    "revision": "7331139d2cf082cd1ce3d6319459a2a1",
+    "url": "/portfolio/static/media/ic-sun.7331139d.svg"
+  },
+  {
+    "revision": "f848837158975307dff720958dca7851",
+    "url": "/portfolio/static/media/img404.f8488371.png"
+  }
+]);
