e39d614faa8293260bacafe1c46113aa6dff54e0
Mai Trịnh <mrlufydl@gmail.com>
2020-03-23 20:08:07 +0700

    123

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    /* Google */
-    baseURL: `https://mtr-data-portfolio-261706.appspot.com/`
+  /* Google */
+  // baseURL: `https://mtr-data-portfolio-261706.appspot.com/`
+  baseURL: `http://5cee0e3b1c2baf00142cb9e6.mockapi.io/api/`
 });
