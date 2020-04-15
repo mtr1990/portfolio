cf5d53d565261c18e157ad53118eee26d03f6a25
Mai Trịnh <mrlufydl@gmail.com>
2020-04-15 18:18:39 +0700

    sds

--- client/src/config/api.config.js
+++ client/src/config/api.config.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // DEV
-  baseURL: `/api/`,
+//   baseURL: `/api/`,
   // PRODUCTION
-//   baseURL: `https://mtr-backend.herokuapp.com/api/`,
+  baseURL: `https://mtr-backend.herokuapp.com/api/`,
   // baseURL: `http://5cee0e3b1c2baf00142cb9e6.mockapi.io/api/`,
 });
