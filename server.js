2a581b3929977075b4cf27fce7966ff21dd5e634
Mai Trịnh <mrlufydl@gmail.com>
2020-04-15 18:34:01 +0700

    sds

--- server.js
+++ server.js
@@ -2,28 +2,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const path = require("path");
 const bodyparser = require("body-parser");
 const session = require("express-session");
 const cors = require("cors"); // Access-Control-Allow-Origin
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Connect
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mtr_backend", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on("connected", () => {
-  console.log("Mongoose is connected!!!!");
-});
-
-// Data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(cors()); // Access-Control-Allow-Origin
+app.use(morgan("tiny"));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:2247",
+      "hhttps://mtr-portfolio.herokuapp.com",
+    ],
+    methods: ["GET", "HEAD", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    credentials: true, //allow setting of cookies
+  })
+);
 app.use(
   session({
     secret: "supersecretstring12345!",
@@ -37,8 +36,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// HTTP request logger
-app.use(morgan("tiny"));
+//CONNECT DATABASE
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mtr_backend", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!!");
+});
+
+// HTTP REQUEST
+
 const projectsRoutes = require("./routes/projects");
 const emailsRoutes = require("./routes/emails");
 const categoriesRoutes = require("./routes/categories");
