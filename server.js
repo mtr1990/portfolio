3bfb322009b001651831f3c6173e88fe3bde81a5
Mai Trịnh <mrlufydl@gmail.com>
2020-04-16 02:24:22 +0700

    12

--- server.js
+++ server.js
@@ -45,24 +45,8 @@ app.use(
 );
 
 //HEROKU SETUP
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static('client/build'))
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-//ROUND 2 DEPLOY
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  //
-  app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
-  });
+  app.use(express.static("client/build"));
 }
 
 // HTTP REQUEST
