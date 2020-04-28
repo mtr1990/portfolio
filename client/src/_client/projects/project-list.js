cae6df655ab0259c037629b45a1a2e03a45f313c
Mai Trịnh <mrlufydl@gmail.com>
2020-04-28 16:12:52 +0700

    last

--- client/src/_client/projects/project-list.js
+++ client/src/_client/projects/project-list.js
@@ -2,10 +2,7 @@ import React from "react";
 import { ProjectItem } from "..";
 
 const ProjectList = ({ stateProject }) => {
-  if (!stateProject.length) return null;
-
-  console.log("LIST RENDER");
-
+  if (stateProject.length === 0) return null;
   return stateProject.map((item) => <ProjectItem key={item._id} item={item} />);
 };
 
