5fdce996b61233283ff14b7113aa7efc6db8e54b
Mai Trịnh <mrlufydl@gmail.com>
2020-05-19 14:43:06 +0700

    ok

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -7,7 +7,8 @@ const config = process.env.NODE_ENV === "development" ? dev : prod;
 console.log("config", config);
 
 const API = axios.create({
-  baseURL: config,
+  // baseURL: config,
+  baseURL: `https://mtr-portfolio.herokuapp.com/api/`,
 });
 
 API.defaults.withCredentials = true;
